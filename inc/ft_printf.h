a7535df4ea5d0506731a7dad39731c72cf707aaf
Mehdi <zaiicko7@proton.me>
2024-05-02 19:24:18 +0200

    norm

--- inc/ft_printf.h
+++ inc/ft_printf.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zaiicko <meskrabe@student.s19.be>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/02 19:22:22 by zaiicko           #+#    #+#             */
+/*   Updated: 2024/05/02 19:23:18 by zaiicko          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
@@ -5,7 +17,7 @@
 # include "stdarg.h"
 # include "stdio.h"
 
-int		ft_printf(const char *, ...);
+int		ft_printf(const char *str, ...);
 int		ft_putchar_f(char c);
 size_t	ft_checking(va_list arg, char c);
 

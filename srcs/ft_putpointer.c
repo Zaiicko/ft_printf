99285c2b903089f3fb2f13da5e17a2efce187724
Mehdi <zaiicko7@proton.me>
2024-05-11 00:35:30 +0200

    correc for %p int

--- srcs/ft_putpointer.c
+++ srcs/ft_putpointer.c
@@ -6,7 +6,7 @@
 /*   By: zaiicko <meskrabe@student.s19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 20:54:28 by zaiicko           #+#    #+#             */
-/*   Updated: 2024/05/09 03:06:53 by zaiicko          ###   ########.fr       */
+/*   Updated: 2024/05/11 00:32:59 by zaiicko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,7 @@ int	ft_putpointer(void *ptr, char *str)
 	nb = (size_t)ptr;
 	base = ft_strlen(str);
 	i = ft_putstr_f("0x");
-	ft_putnbase(nb / base, str);
-	ft_putnbase(nb % base, str);
+	ft_putnbase(nb, str);
 	while (nb > 0)
 	{
 		nb = nb / base;
